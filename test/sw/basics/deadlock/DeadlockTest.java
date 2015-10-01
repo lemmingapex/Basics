ee76ce1ccaab55ac8aa5ad601c7e149a9903d21b
Scott Wiedemann <lemmingapex@gmail.com>
2015-10-01 10:57:19 -0600

    deadlock message

--- test/sw/basics/deadlock/DeadlockTest.java
+++ test/sw/basics/deadlock/DeadlockTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 public class DeadlockTest {
 
 	@Test
-	public void test() {
+	public void test() throws InterruptedException {
 		BlockingQueue<Runnable> q = new LinkedBlockingQueue<Runnable>();
 		for(int i = 0; i < 100; i++){
             q.add(new Deadlock(i));
@@ -18,5 +18,7 @@ public class DeadlockTest {
 		
 		ThreadPoolExecutor pool = new ThreadPoolExecutor(2, 100, 1, TimeUnit.MINUTES, q);
 		pool.prestartAllCoreThreads();
+		pool.awaitTermination(Long.MAX_VALUE, TimeUnit.MILLISECONDS);
+		System.out.println("Never run");
 	}
 }
